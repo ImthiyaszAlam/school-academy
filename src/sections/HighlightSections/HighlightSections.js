6b2d962fb6ed18ac8acaff60a2be7bc6e156f759
Imthiyas Alam <developerimthiyas+imthiyas@gmail.com>
2026-03-24 11:05:40 +0530

    ImageBackground in HighlightSection

--- src/sections/HighlightSections/HighlightSections.js
+++ src/sections/HighlightSections/HighlightSections.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, useWindowDimensions } from 'react-native';
+import { View, Text, useWindowDimensions, ImageBackground } from 'react-native';
 import SectionWrapper from '../../components/SectionWrapper/SectionWrapper';
 import SectionTopBar from '../../components/SectionTopBar/SectionTopBar';
 import { highlightData } from '../../content/highlightData';
@@ -11,33 +11,38 @@ const HighlightSections = () => {
   const isMobile = width < 768;
 
   return (
-    <SectionWrapper style={styles.container}>
-      <SectionTopBar
-        title={highlightSectionHeader.title}
-        heading={highlightSectionHeader.heading}
-        subTitle={highlightSectionHeader.subTitle}
-        align={highlightSectionHeader.align}
-      />
+    <ImageBackground
+      source={{ uri: 'https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=1600&q=80' }}
+      style={styles.container}
+    >
+      <SectionWrapper style={styles.container}>
+        <SectionTopBar
+          title={highlightSectionHeader.title}
+          heading={highlightSectionHeader.heading}
+          subTitle={highlightSectionHeader.subTitle}
+          align={highlightSectionHeader.align}
+        />
 
-      {/* Highlight Cards Grid */}
-      <View style={styles.highlightGrid}>
-        {highlightData.map((highlight) => (
-          <View
-            key={highlight.id}
-            style={[
-              styles.highlightCard,
-              isMobile && styles.highlightCardMobile,
-            ]}
-          >
-            <Text style={styles.iconContainer}>{highlight.icon}</Text>
-            <Text style={styles.highlightTitle}>{highlight.title}</Text>
-            <Text style={styles.highlightDescription}>
-              {highlight.description}
-            </Text>
-          </View>
-        ))}
-      </View>
-    </SectionWrapper>
+        {/* Highlight Cards Grid */}
+        <View style={styles.highlightGrid}>
+          {highlightData.map((highlight) => (
+            <View
+              key={highlight.id}
+              style={[
+                styles.highlightCard,
+                isMobile && styles.highlightCardMobile,
+              ]}
+            >
+              <Text style={styles.iconContainer}>{highlight.icon}</Text>
+              <Text style={styles.highlightTitle}>{highlight.title}</Text>
+              <Text style={styles.highlightDescription}>
+                {highlight.description}
+              </Text>
+            </View>
+          ))}
+        </View>
+      </SectionWrapper>
+    </ImageBackground>
   );
 };
 
